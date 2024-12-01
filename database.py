aa9a61719670c6f0c4db33d7f216236313f544d7
Valter <sv1801198@gmail.com>
2024-12-01 11:34:04 -0300

    Homepage e páginas de cadastro adicionadas

---
+++ database.py
@@ -0,0 +1,55 @@
+import peewee
+from peewee import IntegrityError
+
+database = peewee.MySQLDatabase(
+    'anaiza',
+    user='root',
+    password='1234',
+    host='localhost',
+    port=3306
+)
+
+class Empresa(peewee.Model):
+    #nome = peewee.CharField()
+    #login = peewee.CharField(unique=True)
+    senha = peewee.CharField()
+    email = peewee.CharField(unique=True)
+    telefone = peewee.CharField()
+
+    class Meta:
+        database = database
+
+class Produto(peewee.Model):
+    nome = peewee.CharField()
+    preco = peewee.DecimalField(max_digits=10, decimal_places=2)
+    img_link = peewee.CharField()
+    descricao = peewee.CharField(null=True)
+    empresa = peewee.ForeignKeyField(Empresa, backref='produtos')
+
+    class Meta: 
+        database = database
+
+class Categorias(peewee.Model):
+    nome = peewee.CharField()
+    empresa = peewee.ForeignKeyField(Empresa, backref='categorias')
+
+    class Meta: 
+        database = database
+
+class Perfil(peewee.Model):
+    nome = peewee.CharField(null=True)
+    endereco = peewee.CharField(null=True)
+    telefone1 = peewee.CharField(null=True)
+    telefone2 = peewee.CharField(null=True)
+    telefone3 = peewee.CharField(null=True)
+    email = peewee.CharField(null=True)
+    horario = peewee.CharField(null=True)
+    empresa = peewee.ForeignKeyField(Empresa, backref='perfil')
+
+    class Meta: 
+        database = database
+
+def create_tables():
+    database.create_tables([Empresa], safe=True)
+    database.create_tables([Produto], safe=True)
+    database.create_tables([Categorias], safe=True)
---
+++ main.py
@@ -0,0 +1,77 @@
+from database import *
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+create_tables()
+
+@app.get("/")
+async def get_home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/cadastro_empresa")
+async def get_create_empresa(request: Request):
+    return templates.TemplateResponse("cadastrar_empresa.html", {"request": request})
+
+@app.post("/cadastro_empresa")
+async def post_create_empresa(request: Request):
+    form_data = await request.form()
+    nova_empresa = Empresa(
+        nome=form_data['nome'],
+        login=form_data['login'],
+        senha=form_data['senha'],
+        email=form_data['email'],
+        telefone=form_data['telefone'],
+    )
+    try:
+        nova_empresa.save()
+        return {"message": "Empresa cadastrada com sucesso!"}
+    except IntegrityError as e:
+        return {"error": str(e)}
+    except:
+        return {"error": "um erro inesperado ocorreu"}
+
+@app.get("/cadastro_categoria")
+async def get_cadastro_categoria(request: Request):
+    return templates.TemplateResponse("cadastrar_categoria.html", {"request": request})
+
+@app.post("/cadastro_categoria")
+async def post_cadastro_categoria(request: Request):
+    form_data = await request.form()
+    nova_categoria = Categorias(
+        nome=form_data['nome'],
+        empresa=form_data['empresa']
+    )
+    try:
+        nova_categoria.save()
+        return {"message": "Categoria cadastrada com sucesso!"}
+    except IntegrityError as e:
+        return {"error": str(e)}
+    except:
+        return {"error": "um erro inesperado ocorreu"}
+
+@app.get("/cadastro_produto")
+async def get_create_produto(request: Request):
+    return templates.TemplateResponse("cadastrar_produto.html", {"request": request})
+
+@app.post("/cadastro_produto")
+async def post_cadastro_produto(request: Request):
+    form_data = await request.form()
+    novo_produto = Produto(
+        nome=form_data['nome'],
+        preco=form_data['preco'],
+        descricao=form_data['descricao'],
+        img_link=form_data['img_link'],
+        empresa=form_data['empresa']
+    )
+    try:
+        novo_produto.save()
+        return {"message": "Produto cadastrado com sucesso!"}
+    except IntegrityError as e:
+        return {"error": str(e)}
+    except:
+        return {"error": "um erro inesperado ocorreu"}
